6a5bdf2cc2f23f72b1b6968a729b1897512c07dc
s3678460 <s3678460@rmit.edu.vn>
2018-11-02 13:40:59 +0700

    commit by khanh

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavigationBar from '../NavigationBar/NavigationBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavigationBar/>
+      </div>
+    );
+  }
+}
+
+export default App;
