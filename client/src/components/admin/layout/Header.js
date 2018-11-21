131087671b3825f3141ff01509e86d63c8e8718d
Linh Do <s3689251@rmit.edu.vn>
2018-11-21 16:45:00 +0700

    admin-routing

--- client/src/components/admin/layout/Header.js
+++ client/src/components/admin/layout/Header.js
@@ -1,18 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">
+        <a href="/admin" className="navbar-brand">
           PickFrame Admin
         </a>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a href="/" className="nav-link">
-                Home
-              </a>
+              <Link to="/admin" className="nav-link">
+                <i className="fas fa-warehouse" /> Orders
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/admin/contributors" className="nav-link">
+                <i className="fas fa-user-ninja" /> Contributors
+              </Link>
             </li>
           </ul>
         </div>
