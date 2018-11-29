51d3c179adbce2946960676f96f07713709c9ca4
Huynh Cong Minh <huynhcongminh@Huynhs-MBP.net.fpt>
2018-11-29 23:56:23 +0700

    commit

--- routes/api/Users.js
+++ routes/api/Users.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const router = express.Router();
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const keys = require("../../config/keys")
+const passport = require("passport");
 
-//User model
+
+//Load input validation
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput =require('../../validation/login')
+
+//Load User model
 const User = require("../../models/User")
 
 
@@ -9,38 +18,60 @@ const User = require("../../models/User")
 //@desc GET ALL users
 //@access Public
 
-router.get("/", (req,res)=>{
+router.get("/", (req, res) => {
     User.find()
-    .then(users => res.json(users))
+        .then(users => res.json(users))
 })
 
 
-//@route POST api/user
-//@desc Create an user
+//@route POST api/user/register
+//@desc Register an user
 //@access Public
 
-router.post('/', (req,res)=>{
-    const newUser = new User({
-        fullName: req.body.fullName,
-        displayName: req.body.displayName,
-        email: req.body.email,
-        password: req.body.password,
-        accountHolder: req.body.accountHolder,
-        cardNumber: req.body.cardNumber,
-        bankName: req.body.bankName,
-        bankBranch: req.body.bankBranch
+router.post('/register', (req, res) => {
+    const {errors, isValid} =validateRegisterInput(req.body);
+
+    if(!isValid){
+        return res.status(400).json(errors)
+    }    
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            errors.email = "Email already exists"
+            return res.status(400).json(errors)
+        } else {
+            const newUser = new User({
+                fullName: req.body.fullName,
+                displayName: req.body.displayName,
+                email: req.body.email,
+                password: req.body.password,
+                accountHolder: req.body.accountHolder,
+                cardNumber: req.body.cardNumber,
+                bankName: req.body.bankName,
+                bankBranch: req.body.bankBranch
+            });
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save().then(user => res.json(user))
+                        .catch(err => console.log(err));
+                })
+            })
+
+        }
     })
-    newUser.save().then(user => res.json(user))
+
+
 })
 
 //@route DELETE api/user
 //@desc Delete an user
 //@access Public
 
-router.delete('/:_id', (req, res)=>{
+router.delete('/:_id', (req, res) => {
     User.findByIdAndRemove(req.params._id)
-    .then(removedUser => res.send(removedUser))
-    .catch(err => res.status(404).json({success: false}))
+        .then(removedUser => res.send(removedUser))
+        .catch(err => res.status(404).json({ success: false }))
 })
 
 
@@ -48,21 +79,81 @@ router.delete('/:_id', (req, res)=>{
 //@desc update an user
 //@access Public
 
-router.put('/:_id', (req, res)=>{
+router.put('/:_id', (req, res) => {
     var update = req.body;
     User.findByIdAndUpdate(req.params._id, update)
-    .then(()=> res.json({update: true}))
-    .catch(err => res.status(404).json({update: false}))
+        .then(() => res.json({ update: true }))
+        .catch(err => res.status(404).json({ update: false }))
 })
 
-//@route GET api/user
-//@desc GET an user
+
+
+
+//@route GET api/users/login
+//@desc Login User / Returning JWT Token
 //@access Public
 
-router.get('/:_id', (req, res)=>{
-    User.findById(req.params._id)
-    .then(user => res.json(user))
-    .catch(err => res.status(404).json({get: false}))
+router.post('/login', (req, res) => {
+
+    const {errors, isValid} =validateLoginInput(req.body);
+
+    if(!isValid){
+        return res.status(400).json(errors)
+    }    
+
+    
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find user by email
+
+    User.findOne({ email }).then(user => {
+        //Check for user
+        if (!user) {
+            errors.email ="User not found"
+            return res.status(404).json(errors);
+        }
+
+        //Check for password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                //User Matched
+                const payload = { id: user.id, fullName: user.fullName, displayName: user.displayName, } //Create JWT payload
+                //Sign Token
+                jwt.sign(payload,
+                    keys.secretOrKey,
+                    { expiresIn: 3600 },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    });
+            } else {
+                errors.password="Password incorrect"
+                return res.status(400).json(errors)
+            }
+        });
+    })
+});
+
+//@route GET api/users/current
+//@desc Return current user
+//@access Private
+
+router.get("/current", passport.authenticate("jwt", { session: false }), (req, res) => {
+
+    res.json({
+        id:req.user.id,
+        fullName: req.user.fullName,
+        displayName: req.user.displayName,
+        email: req.user.email,
+        accountHolder: req.user.accountHolder,
+        cardNumber: req.user.cardNumber,
+        bankName: req.user.bankName,
+        bankBranch: req.user.bankBranch
+
+    })
 })
 
 module.exports = router;
