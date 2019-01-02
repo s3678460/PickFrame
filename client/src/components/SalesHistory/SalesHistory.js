59fbb0ad51349d3951fe20b0c44b879f04a28c90
AnhTu <anhtu.bui120491@gmail.com>
2019-01-02 13:10:35 +0700

    update background

--- client/src/components/SalesHistory/SalesHistory.js
+++ client/src/components/SalesHistory/SalesHistory.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import bgImage from "../../images/demo4.jpg"
+import bgImage from "../../images/salebg.jpg"
 
 import { Table } from 'reactstrap';
 import { Animation } from "mdbreact"
